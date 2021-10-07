e4ae07e5f4ae2ef68d237dbe4549c736f537481f
ANIRUDH <anirudhsudheer@gmail.com>
2021-10-07 21:06:33 +0530

    Update resetlimit.js

--- plugins/resetlimit.js
+++ plugins/resetlimit.js
@@ -2,10 +2,10 @@ let { Presence } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, args }) => {
 	await conn.updatePresence(m.chat, Presence.composing) 
 	let list = Object.entries(global.DATABASE.data.users)
-	let lim = !args || !args[0] ? 100 : isNumber(args[0]) ? parseInt(args[0]) : 100
+	let lim = !args || !args[0] ? 1000 : isNumber(args[0]) ? parseInt(args[0]) : 100
 	lim = Math.max(1, lim)
 	list.map(([user, data], i) => (Number(data.limit = lim)))
-		conn.reply(m.chat, `*berhasil direset ${lim} / user*`, m)
+		conn.reply(m.chat, `*reset successfully ${lim} / user*`, m)
 }
 handler.help = ['limit'].map(v => 'reset' + v)
 handler.tags = ['owner', 'host']
